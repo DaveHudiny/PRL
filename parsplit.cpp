154e2b55ebd7d72a565cfc52b9c4a84680cdfd2f
David Hudák <61205638+DaveHudiny@users.noreply.github.com>
2023-03-24 13:19:29 +0100

    Created some weird stuff

--- parsplit.cpp
+++ parsplit.cpp
@@ -7,12 +7,49 @@
  * 
  */
 
-#include<iostream>
+#include <iostream>
+#include <mpi.h>
+
+#define MAXSIZE 64
+#define MINSIZE 8
 
 using namespace std;
 
-int main()
+void root_process(unsigned array[])
 {
-    cout << "Ahoj!" << endl; 
+    unsigned char byte;
+    int i = 0;
+    FILE* file = fopen("numbers", "r");
+    if (file == NULL) {
+        printf("Nepovedlo se otevrit soubor\n");
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+    
+    while (fscanf(file, "%c", &byte) != EOF && i < 64) {
+        array[i] = unsigned(byte);
+        cout << unsigned(byte) << endl;
+        i++;
+    }
+    fclose(file);
+}
+
+int main(int argc, char **argv)
+{
+    int rank;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rank == 0) {
+        int input_array[MAXSIZE];
+        root_process(input_array);
+        cout << "Ja jsem root!" << endl; 
+    }
+    else
+    {
+        cout << "Ahoj!" << endl; 
+        
+    }
+
+    MPI_Finalize();
     return 0;
+    
 }
